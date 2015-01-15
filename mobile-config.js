18a953669ffb627aff996b6fe653802912b13093
paul.sturm <paul.sturm@project-a.com>
2015-01-15 20:33:57 +0100

    added android platform

---
+++ mobile-config.js
@@ -0,0 +1,10 @@
+
+App.info({
+  id: 'de.wbcrew.android.lunchtime',
+  name: 'lunchtime',
+  description: 'small lunchtime app',
+  author: 'rieL',
+  email: 'rieL@wbcrew.de',
+  website: 'http://lunchtime.meteor.com'
+});
+
